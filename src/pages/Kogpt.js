0c885d5bebf1e33728b60947e72a60d7170c81b1
mike7051141 <mike705114@gmail.com>
2023-12-02 19:58:57 +0900

    kogpt

---
+++ src/pages/Kogpt.js
@@ -0,0 +1,54 @@
+// papago.js
+import { useState } from "react";
+import axios from "axios";
+
+const Kogpt = () => {
+  const [inputText, setInputText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+
+  const makeStroy = async () => {
+    try {
+      const data = JSON.stringify({
+        prompt: inputText,
+        max_tokens: 512,
+        temperate: 0.1,
+        top_p: 0.1,
+        n: 1,
+      });
+
+      const config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "/v1/inference/kogpt/generation",
+        headers: {
+          Authorization: "KakaoAK 4eb86019e227977251af1441c837fb0c",
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      const response = await axios.request(config);
+      setTranslatedText(inputText + response.data.generations[0].text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div>
+        <textarea
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="스토리의 초기부분을 작성해주세요 :"
+          rows={4}
+          style={{ width: "100%" }}
+        />
+      </div>
+      <button onClick={makeStroy}>스토리 생성</button>
+      <p>번역된 텍스트: {translatedText}</p>
+    </div>
+  );
+};
+
+export default Kogpt;
---
+++ src/pages/Main.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import "../style.css";
+const Main = () => {
+  const title = "웹 개발 응용 (화) 프로젝트";
+  const [imageSrc, setImageSrc] = useState("kangnam_logo.png");
+  const [kangnamInfo, setKangnamInfo] = useState({
+    name: "",
+    id: "",
+    intro: "",
+  });
+
+  const names = ["김경모", "김민", "김민기"];
+  const srcs = ["kimmo.jpg", "kimmin.jpg", "kimmingi.jpg"];
+  const kangnamId = ["201904011", "201904016", "201904017"];
+  const kangnamIntro = [
+    "안녕하세요 ICT융합공학부 19학번 김경모입니다.",
+    "안녕하세요 소프트웨어응용학부 19학번 김민입니다.",
+    "안녕하세요 소프트웨어응용학부 19학번 김민기입니다.",
+  ];
+
+  const handleImageChange = (index) => {
+    setImageSrc(srcs[index]);
+    setKangnamInfo({
+      name: names[index],
+      id: kangnamId[index],
+      intro: kangnamIntro[index],
+    });
+  };
+
+  return (
+    <div
+      className="center"
+      style={{
+        width: "100%",
+        height: 1100,
+        backgroundImage: 'url("/back2.jpg")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 style={{ textAlign: "center", color: "black" }}>{title}</h1>
+      <table>
+        <tr>
+          <td id="td2">
+            <img
+              alt="pic"
+              className="zoom round"
+              id="imagekangnam"
+              src={imageSrc}
+              width={250}
+            />
+          </td>
+          <td>
+            <table id="grid">
+              <tbody>
+                {names.map((name, index) => (
+                  <tr key={index}>
+                    <td className="td-left">
+                      <img
+                        alt="pic"
+                        className="ChangeImage"
+                        width={60}
+                        onClick={() => handleImageChange(index)}
+                        src={srcs[index]}
+                      />
+                    </td>
+                    <td className="td-right">
+                      <span>{name}</span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <td width="100px" className="hide">
+            이름:
+          </td>
+          <td width="300px">
+            <span id="knames">{kangnamInfo.name}</span>
+          </td>
+        </tr>
+        <tr>
+          <td className="hide">학번:</td>
+          <td>
+            <span id="kangnamID">{kangnamInfo.id}</span>
+          </td>
+        </tr>
+        <tr>
+          <td className="hide">소개:</td>
+          <td>
+            <span id="kangnamIntro">{kangnamInfo.intro}</span>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default Main;
