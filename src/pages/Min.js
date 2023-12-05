6e02660b95b451ab2584b78ec403178835f1e358
201904016 <rlaals7760@daum.net>
2023-12-05 13:23:12 +0900

    노래

--- src/pages/Min.js
+++ src/pages/Min.js
@@ -10,15 +10,18 @@ const Min = () => {
   const [inputsearch, setinputSearch] = useState("");
   const [searchdata, setSearchdata] = useState("");
   const [storedToken, setStoredToken] = useState("");
+  const [albums, setAlbums] = useState([]);
 
   const getProfile = async () => {
     try {
+      const Id = "ee6178387d344c0da243b195c99b34c4";
+      const Password = "37d2be423dbf4563b06394127f887b75";
       const response = await axios.post(
         "https://accounts.spotify.com/api/token",
         "grant_type=client_credentials&client_id=" +
-          inputid +
+          Id +
           "&client_secret=" +
-          inputpassword,
+          Password,
         {
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -60,10 +63,9 @@ const Min = () => {
     try {
       const response = await axios.get("https://api.spotify.com/v1/search", {
         params: {
-          q: "remaster track:Doxy artist:Miles Davis",
+          q: inputsearch,
           type: "album",
-          market: "ES",
-          limit: 10,
+          limit: 20,
           offset: 5,
           include_external: true,
         },
@@ -73,9 +75,21 @@ const Min = () => {
       });
 
       console.log(response.data);
-      setSearchdata(
-        response.data.albums.items.map((album) => album.name).join(",")
-      );
+
+      const albums = response.data.albums.items.map((album) => ({
+        name: album.name,
+        artists: album.artists.map((artist) => artist.name).join(", "),
+        album_type: album.album_type,
+        external_urls: album.external_urls.spotify,
+        album_href: album.href,
+        images: album.images.map((image) => image.url),
+        is_playable: album.is_playable,
+        release_date: album.release_date,
+        total_tracks: album.total_tracks,
+      }));
+
+      console.log(albums);
+      setAlbums(albums); // 앨범 정보를 state 또는 변수에 저장할 수 있습니다.
     } catch (error) {
       console.error(error);
     }
@@ -111,7 +125,26 @@ const Min = () => {
         ></textarea>
       </div>
       <button onClick={Searchsong}>노래검색</button>
-      <p style={{ color: "darkblue", fontSize: 16 }}>{searchdata}</p>
+      <div>
+        {albums.map((album, index) => (
+          <div key={index} style={{ display: "flex", marginBottom: 20 }}>
+            <img
+              src={album.images[0]}
+              alt="Album Thumbnail"
+              style={{ maxWidth: "150px", marginRight: 20 }}
+            />
+
+            <div style={{ flex: 1 }}>
+              <h2 style={{ fontSize: 20 }}>{album.name}</h2>
+              <p>{`아티스트: ${album.artists}`}</p>
+              <p>{`앨범 타입: ${album.album_type}`}</p>
+              <p>{`출시 날짜: ${album.release_date}`}</p>
+              <p>{`트랙 수: ${album.total_tracks}`}</p>
+              {/* <button onClick={() => addList(album)}>Add to toReadList</button> */}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
