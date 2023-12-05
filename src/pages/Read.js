32aedde5211f8caacdb1dd97b16f64774e9ac347
mike7051141 <mike705114@gmail.com>
2023-12-05 13:19:20 +0900

    반갈

--- src/pages/Read.js
+++ src/pages/Read.js
@@ -21,6 +21,7 @@ const Read = () => {
         thumbnail: document.thumbnail,
         finishDate: document.finishDate,
         hasRead: document.hasRead,
+        id: document.id,
       }));
       console.log("Mapped Book Data:", bookData);
       // Update state with the mapped book data
@@ -30,6 +31,7 @@ const Read = () => {
     }
   };
   const finishReading = async (documentId) => {
+    console.log(documentId);
     try {
       // Make a request to update the hasRead value for the clicked document
       await axios.patch(`http://localhost:4000/documents/${documentId}`, {
@@ -42,39 +44,75 @@ const Read = () => {
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <button onClick={searchBook}>Search toReadList</button>
-      {/* 책 정보들 매핑해서 출력하기 */}
-      {books.map((book, index) => (
-        <div key={index} style={{ display: "flex", marginBottom: 20 }}>
-          {/* 썸네일 */}
-          <img
-            src={book.thumbnail}
-            alt="Thumbnail"
-            style={{ maxWidth: "150px", marginRight: 20 }}
-            onClick={() => finishReading(book.id)}
-          />
+    <div style={{ display: "flex" }}>
+      <div style={{ padding: 20 }}>
+        <button onClick={searchBook}>Search toReadList</button>
+        {/* 책 정보들 매핑해서 출력하기 */}
+        {books.map((book, index) => (
+          <div key={index} style={{ display: "flex", marginBottom: 20 }}>
+            {/* 썸네일 */}
+            <img
+              src={book.thumbnail}
+              alt="Thumbnail"
+              style={{ maxWidth: "150px", marginRight: 20 }}
+              onClick={() => finishReading(book.id)}
+            />
 
-          {/* 책 제목, 내용, 가격, 출판사 등등 */}
-          <div>
-            <h2 style={{ fontSize: 20 }}>{book.title}</h2>
-            <p
-              style={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitBoxOrient: "vertical",
-                WebkitLineClamp: 3,
-              }}
-            >
-              {book.contents}
-            </p>
-            <p>가격: {book.price}원</p>
-            <p>출판사: {book.publisher}</p>
-            <p>{book.finishDate}까지 읽을 예정이시죠?</p>
+            {/* 책 제목, 내용, 가격, 출판사 등등 */}
+            <div>
+              <h2 style={{ fontSize: 20 }}>{book.title}</h2>
+              <p
+                style={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitBoxOrient: "vertical",
+                  WebkitLineClamp: 3,
+                }}
+              >
+                {book.contents}
+              </p>
+              <p>가격: {book.price}원</p>
+              <p>출판사: {book.publisher}</p>
+              <p>{book.finishDate}까지 읽을 예정이시죠?</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div style={{ padding: 20 }}>
+        <button onClick={searchBook}>Search toReadList</button>
+        {/* 책 정보들 매핑해서 출력하기 */}
+        {books.map((book, index) => (
+          <div key={index} style={{ display: "flex", marginBottom: 20 }}>
+            {/* 썸네일 */}
+            <img
+              src={book.thumbnail}
+              alt="Thumbnail"
+              style={{ maxWidth: "150px", marginRight: 20 }}
+              onClick={() => finishReading(book.id)}
+            />
+
+            {/* 책 제목, 내용, 가격, 출판사 등등 */}
+            <div>
+              <h2 style={{ fontSize: 20 }}>{book.title}</h2>
+              <p
+                style={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitBoxOrient: "vertical",
+                  WebkitLineClamp: 3,
+                }}
+              >
+                {book.contents}
+              </p>
+              <p>가격: {book.price}원</p>
+              <p>출판사: {book.publisher}</p>
+              <p>{book.finishDate}까지 읽을 예정이시죠?</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
